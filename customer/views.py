555f049bb07df85fb2bed27867300264f9aba924
HenryC0416 <henrychen0416@gmail.com>
2024-07-24 15:00:53 -0400

    Add files via upload

---
+++ customer/views.py
@@ -0,0 +1,41 @@
+
+from django.shortcuts import render
+from django.views import View 
+from django.views import generic
+
+from customer.models import MenuItem
+# Create your views here.
+
+class Index(View):
+    def get(self, request, *args,**kwarfgs):
+        return render(request, "customer/index.html")
+
+
+
+class Menu(generic.ListView):
+    model = MenuItem
+    context_object_name = 'menu_list'   
+    queryset = MenuItem.objects.all()
+    template_name = 'customer/menu.html'  
+
+class Order(View): 
+    model=MenuItem
+    context_object_name='food_list'
+    queryset = non_coffee=MenuItem.objects.filter(category__name__contains='Food')
+    template_name = 'customer/menu.html'  
+
+    def get(self, request, *args,**kwarfgs):
+        non_coffee=MenuItem.objects.filter(category__name__contains='Non-Coffee')
+        coffee=MenuItem.objects.filter(category__name__contains='Coffee')
+        food=MenuItem.objects.filter(category__name__contains='Food')
+    
+
+        context = {
+            'non_coffee':non_coffee,
+            'coffee':coffee,
+            'food':food,
+        }
+
+        return render(request, 'customer/menu.html',context)
+    
+   
---
+++ main/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser , Group
+
+
+
+
+
+
+class Branch(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=200, blank=True, null=True)
+    zip_code = models.CharField(max_length=10,blank=True, null=True)  
+    
+
+    def __str__(self):
+        return self.name
+
+class CustomUser(AbstractUser): 
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('customer', 'Customer'),
+        ('uber-user', 'Uber-User')
+    ]
+
+    
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='customer')    
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, null=True, blank=True)
+
+    is_active = models.BooleanField(default=True)
+    groups = models.ManyToManyField(Group, related_name='customuser_set', blank=True)
+   
+
+    def save(self, *args, **kwargs):
+        if self.is_superuser:
+            self.role = 'uber-user'
+            self.is_staff = True
+        elif self.role in ['admin', 'uber-user']:
+            self.is_staff = True
+        else:
+            self.is_staff = False
+        super(CustomUser, self).save(*args, **kwargs)
+
+
