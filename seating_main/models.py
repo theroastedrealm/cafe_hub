d9d3c882a1d7c6c6fa90761479466f7f174b11ad
Shevchikk <igorgrygoriv1@gmail.com>
2024-07-30 10:20:01 -0400

    stuff

--- seating_main/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.conf import settings
-
-# Create your models here.
-class Seat(models.Model):
-    name = models.CharField(max_length=15)
-    available = models.BooleanField(default=True)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
