d444fe3de24a7f7e7222ed1d9fa92598f952a50c
HenryC <henrychen0416@gmail.com>
2024-08-15 13:44:17 -0400

    added branch filtering to specials app

--- cafe_hub/specials/admin.py
+++ cafe_hub/specials/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
+
+from main.models import Branch
 from .models import Special
 # Register your models here.
-admin.site.register(Special)
+class SpecialsAdmin(admin.ModelAdmin):
+    list_display=['name','description','branch']
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(branch=request.user.branch)
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        if not request.user.is_superuser:
+            form.base_fields['branch'].queryset = Branch.objects.filter(id=request.user.branch.id)
+        return form
+admin.site.register(Special, SpecialsAdmin)
