98a8c38549d8dadc7eeace9204ec7c10aae6250d
HenryC <henrychen0416@gmail.com>
2024-08-21 22:48:37 -0400

    UI updates to playlist and cafeposts

--- cafe_hub/cafePosts/admin.py
+++ cafe_hub/cafePosts/admin.py
@@ -1,6 +1,71 @@
 from django.contrib import admin
+
+from main.models import Branch
 from .models import Post, Comment, UserProfile
 
-admin.site.register(Post)
-admin.site.register(Comment)
-admin.site.register(UserProfile)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['author', 'title', 'content', 'created_at', 'updated_at','branch']
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        
+        if request.user.branch:
+            return qs.filter(id=request.user.branch.id)
+        else:
+            return qs.none()
+
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        if not request.user.is_superuser:
+            form.base_fields['branch'].queryset = Branch.objects.filter(id=request.user.branch.id)
+        return form
+    
+admin.site.register(Post, PostAdmin)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['post','author', 'content', 'created_at', 'updated_at','branch']
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        
+        if request.user.branch:
+            return qs.filter(id=request.user.branch.id)
+        else:
+            return qs.none()
+
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        if not request.user.is_superuser:
+            form.base_fields['branch'].queryset = Branch.objects.filter(id=request.user.branch.id)
+        return form
+    
+admin.site.register(Comment, CommentAdmin)
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'bio', 'birth_date', 'profile_pic', 'created_at']
+    
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        
+        if request.user.branch:
+            return qs.filter(id=request.user.branch.id)
+        else:
+            return qs.none()
+
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        if not request.user.is_superuser:
+            form.base_fields['branch'].queryset = Branch.objects.filter(id=request.user.branch.id)
+        return form
+    
+admin.site.register(UserProfile, UserProfileAdmin)
--- cafe_hub/cafePosts/models.py
+++ cafe_hub/cafePosts/models.py
@@ -1,13 +1,15 @@
 from django.conf import settings
 from django.db import models
 
+from main.models import Branch
+
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, null=False, blank=False, default=1)
     def __str__(self):
         return self.title
 
@@ -17,7 +19,7 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE, null=False, blank=False, default=1)
     def __str__(self):
         return f"Comment by {self.author.username} on {self.post.title}"
 
@@ -27,6 +29,6 @@ class UserProfile(models.Model):
     birth_date = models.DateField(null=True, blank=True)
     profile_pic = models.ImageField(default='default.jpg', upload_to='profile_pics/')
     created_at = models.DateTimeField(auto_now_add=True)
-
+    
     def __str__(self):
         return self.user.username
